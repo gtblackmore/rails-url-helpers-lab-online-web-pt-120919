b7eea25c3edd47900240b226d628b0c3c5f75e99
George Blackmore <gtblackmore@gmail.com>
2020-04-16 03:14:28 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   
   
   get '/students', to: 'students#index'
-  get '/students/:id', to: 'students#show'
+  get '/students/:id/activate', to: 'students#show'
   
   
 end
